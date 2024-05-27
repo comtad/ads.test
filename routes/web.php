f8ebaa38eb91acd36f39e1c3da6a4809a064c83e
comtad <yavahueprosto@gmail.com>
2024-05-27 08:12:03 +0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -5,21 +5,13 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
+    return redirect('/advert');
 });
 
 Route::resource('advert', \App\Http\Controllers\AdverdController::class);
 
 Route::get('advert/get-description/{id}', [\App\Http\Controllers\AdverdController::class, 'getDescription']);
 Route::get('advert/get-more-photos/{id}', [\App\Http\Controllers\AdverdController::class, 'getMorePhotos']);
-
-Route::get('/test', [\App\Http\Controllers\TestController::class, 'index']);
-
 
 
 Route::middleware([
